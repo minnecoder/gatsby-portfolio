b62f00eb6bc4beff70744cc7b8a6bfc9e04c667c
minnecoder <sdparkin@protonmail.com>
2020-08-15 21:44:19 -0500

    Added components and fixed eslint errors

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from '../images/placeimg.jpg';
+
+export default function about() {
+  return (
+    <div>
+      <h2>About Me</h2>
+      {/* <div className="left"><Image /></div>  */}
+
+      <div className="right">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt expedita unde eos numquam, debitis ut nam totam officia fuga mollitia commodi odit neque architecto maxime dolore distinctio quibusdam tenetur facilis reprehenderit dolores illo recusandae? Accusantium, vitae consequuntur reiciendis unde eligendi velit odit minus temporibus nostrum dolor pariatur, nemo quibusdam, exercitationem facilis magni autem! Minima cupiditate iusto commodi, eum earum voluptas inventore deserunt dolor maxime? Iure molestiae aperiam cupiditate quaerat id dolores itaque quidem consequuntur fuga eveniet asperiores, quisquam mollitia, nam doloribus debitis in distinctio maxime voluptatibus modi accusamus! Beatae voluptas molestiae, fugit quisquam fuga odio inventore error nisi tempore architecto.</div>
+    </div>
+  );
+}
---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function main() {
+  return (
+    <div>
+      <p>
+        Hi, my name is Sean Parkin
+        {' '}
+        <br />
+        I am a JavaScript Developer
+      </p>
+    </div>
+  );
+}
