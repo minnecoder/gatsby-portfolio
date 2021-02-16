53a5503ea4cfda96bad4c3555f024128c568db6b
minnecoder <sdparkin@protonmail.com>
2021-02-15 22:06:12 -0600

    Converted to regular component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 
 import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
 import Header from '../components/Header';
 import Home from '../components/Home';
 import Projects from '../components/Projects';
@@ -12,75 +11,18 @@ import Footer from '../components/Footer';
 import './index.css';
 
 const IndexPage = () => (
-  <StaticQuery
-    query={graphql`
-      {
-        bgImage: file(relativePath: { eq: "white-building.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 1600, maxHeight: 800) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        front: allFile(sort: { order: ASC, fields: [id] }, filter: { relativePath: { regex: "icons/front.*.png/" } }) {
-          edges {
-            node {
-              name
-              relativePath
-              childImageSharp {
-                fixed(width: 48, height: 48) {
-                  ...GatsbyImageSharpFixed
-                }
-              }
-            }
-          }
-        }
-        back: allFile(filter: { relativeDirectory: { eq: "icons/back" } }) {
-          edges {
-            node {
-              name
-              relativePath
-              childImageSharp {
-                fixed(width: 48, height: 48) {
-                  ...GatsbyImageSharpFixed
-                }
-              }
-            }
-          }
-        }
-        tools: allFile(filter: { relativeDirectory: { eq: "icons/tools" } }) {
-          edges {
-            node {
-              name
-              relativePath
-              childImageSharp {
-                fixed(width: 48, height: 48) {
-                  ...GatsbyImageSharpFixed
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={(data) => (
-      <Main>
-        <GlobalStyle />
-        <Helmet title="Sean Parkin" />
-        <Home bgImage={data.bgImage} />
-        <Header />
 
-        <About
-          FEimages={data.front.edges}
-          BEimages={data.back.edges}
-          Toolimages={data.tools.edges}
-        />
-        <Projects />
-        <Contact />
-        <Footer />
-      </Main>
-    )}
-  />
+  <Main>
+    <GlobalStyle />
+    <Helmet title="Sean Parkin" />
+    <Home />
+    <Header />
+    <About />
+    <Projects />
+    <Contact />
+    <Footer />
+  </Main>
+
 );
 
 export default IndexPage;
