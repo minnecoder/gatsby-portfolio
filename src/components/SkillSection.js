29aefa6340eda55736a785a48883a73dd954a48f
minnecoder <sdparkin@protonmail.com>
2021-02-23 23:59:13 -0600

    Added key and changed style

--- src/components/SkillSection.js
+++ src/components/SkillSection.js
@@ -2,19 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default function SkillSection({ title, icons }) {
-    return (
-        <div>
-            <SectionTitle>{title}</SectionTitle>
-            <SkillIcons>
-                {icons.map((icon) => (
-                    <SkillItem>
-                        <img src={icon.node.childImageSharp.fixed.src} alt={icon.node.name} />
-                        <p>{icon.node.name}</p>
-                    </SkillItem>
-                ))}
-            </SkillIcons>
-        </div>
-    );
+  return (
+    <div>
+      <SectionTitle>{title}</SectionTitle>
+      <SkillIcons>
+        {icons.map((icon) => (
+          <SkillItem key={icon.node.name}>
+            <img src={icon.node.childImageSharp.fixed.src} alt={icon.node.name} />
+            <p>{icon.node.name}</p>
+          </SkillItem>
+        ))}
+      </SkillIcons>
+    </div>
+  );
 }
 
 const SectionTitle = styled.h3`
@@ -22,7 +22,12 @@ const SectionTitle = styled.h3`
 `;
 const SkillIcons = styled.div`
 display: flex;
+justify-content: center;
 `;
 const SkillItem = styled.div`
 padding: 0 0.5rem;
+img {
+  display: block;
+  margin: auto;
+}
 `;
