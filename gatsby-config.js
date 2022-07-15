9dddf618b61160aee1987dd1dcb1b006701dbcaf
Sparkin <sdparkin@protonmail.com>
2022-07-14 21:16:56 -0500

    Changed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,6 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'icons',
-        path: `${__dirname}/src/images/icons`,
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
         name: 'data',
         path: `${__dirname}/src/data`,
       },
@@ -59,7 +52,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon-32x32.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
